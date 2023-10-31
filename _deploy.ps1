fbcb8e7730fc9411d563c9bf26ecee7279c9e6eb
Padishah <350717997@qq.com>
2023-10-31 16:59:27 +0800

    vault backup: 2023-10-31 16:59:26 by
    Affected files:
    .gitignore
    .obsidian/plugins/obsidian-shellcommands/data.json
    .obsidian/workspace.json
    _deploy.ps1

--- _deploy.ps1
+++ _deploy.ps1
@@ -1,1 +1,1 @@
-chcp 65001;git checkout release;proxychains -f _proxychains.conf hexo g -d --silent;git checkout main
+chcp 65001;git checkout release;proxychains -f ./_proxychains.conf hexo g -d --silent;git checkout main

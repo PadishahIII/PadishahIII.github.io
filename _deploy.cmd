0dab742220e4fe0dcc182ab72305716bee559ec0
Padishah <350717997@qq.com>
2023-10-31 16:06:18 +0800

    vault backup: 2023-10-31 16:06:18 by
    Affected files:
    .obsidian/community-plugins.json
    .obsidian/hotkeys.json
    .obsidian/plugins/obsidian-shellcommands/data.json
    .obsidian/plugins/obsidian-shellcommands/main.js
    .obsidian/plugins/obsidian-shellcommands/manifest.json
    .obsidian/plugins/obsidian-shellcommands/styles.css
    .obsidian/workspace.json
    _deploy.cmd

---
+++ _deploy.cmd
@@ -0,0 +1,3 @@
+git checkout release
+D:\WebPentestProgram\proxychains_0.6.8_win32_x64\proxychains.exe -f D:\WebPentestProgram\proxychains_0.6.8_win32_x64\proxychains.conf hexo g -d
+git checkout main

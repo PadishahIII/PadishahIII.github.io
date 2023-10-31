34602dfb4c4695e496d53a029237634188f83d55
Padishah <350717997@qq.com>
2023-10-31 19:14:32 +0800

    backup

--- _deploy.ps1
+++ _deploy.ps1
@@ -3,6 +3,7 @@ git checkout main
 git add .
 git commit -m "backup"
 git checkout release
+git merge -X theirs main
 proxychains hexo g -d --silent
 git restore .obsidian/workspace*.json
 git stash 

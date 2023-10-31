a86ecc3402d3fb797b28c93b2fecf5744f3713cb
Padishah <350717997@qq.com>
2023-10-31 16:18:42 +0800

    vault backup: 2023-10-31 16:18:41 by
    Affected files:
    .obsidian/workspace.json
    _deploy.cmd
    _proxychains.conf

--- _deploy.cmd
+++ _deploy.cmd
@@ -1,4 +1,4 @@
 chcp 65001
 git checkout release
-D:\WebPentestProgram\proxychains_0.6.8_win32_x64\proxychains.exe -f D:\WebPentestProgram\proxychains_0.6.8_win32_x64\proxychains.conf hexo g -d --silent
+proxychains -f _proxychains.conf hexo g -d --silent
 git checkout main

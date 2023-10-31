0e3f9db08f5865c4a81714a9f02a9edaae0fcc93
Padishah <350717997@qq.com>
2023-10-31 17:02:00 +0800

    backup

--- _deploy.ps1
+++ _deploy.ps1
@@ -1,1 +1,7 @@
-chcp 65001;git checkout release;proxychains -f D:\Files\Note\Note_bak\hexo\_proxychains.conf hexo g -d --silent;git checkout main
+chcp 65001
+git checkout main
+git add .
+git commit -m "backup"
+git checkout release
+proxychains -f D:\Files\Note\Note_bak\hexo\_proxychains.conf hexo g -d --silent
+git checkout main
